3831b0b8cd803b672de9c2641afc010d9b629bf6
AndriyKornytskiy <andriy.kornytskiy@gmail.com>
2021-11-26 01:29:08 +0200

    js hw6 add class work

--- JavaScript_part/hw6/main.js
+++ JavaScript_part/hw6/main.js
@@ -72,36 +72,85 @@ console.log(capitalize1);
 
 //-------------------------------------------------------------class work-------------------------------------------------------------
 // - Дано список імен.
-// let n1 = 'Harry..Potter'
-// let n2 = 'Ron---Whisley'
-// let n3 = 'Hermione__Granger'
+let n1 = 'Harry..Potter'
+let n2 = 'Ron---Whisley'
+let n3 = 'Hermione__Granger'
 // Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
 // let n1 = 'Harry Potter'
 // let n2 = 'Ron Whisley'
 // let n3 = 'Hermione Granger'
-//
-//
+let nameValidator = (str) => {
+    let trim = str.trim();
+    if (trim.includes('.')) {
+        let replaced = trim.replaceAll('.', ' ');
+        let splited = replaced.split('');
+        splited.splice(splited.indexOf(' '), splited.lastIndexOf(' ') - splited.indexOf(' '));
+        return splited.join('');
+    } else if (trim.includes('-')) {
+        let replaced = trim.replaceAll('-', ' ');
+        let splited = replaced.split('');
+        splited.splice(splited.indexOf(' '), splited.lastIndexOf(' ') - splited.indexOf(' '));
+        return splited.join('');
+    } else if (trim.includes('_')) {
+        let replaced = trim.replaceAll('_', ' ');
+        let splited = replaced.split('');
+        splited.splice(splited.indexOf(' '), splited.lastIndexOf(' ') - splited.indexOf(' '));
+        return splited.join('');
+    }
+}
+console.log(nameValidator(n1));
+console.log(nameValidator(n2));
+console.log(nameValidator(n3));
+
 // - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
+let randomiser = (arrLength) => {
+    let arr = [];
+    for (let i = 0; i < arrLength; i++) {
+        arr.push(Math.round(Math.random() * 100));
+    }
+    return arr;
+}
+
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
+let sort = randomiser(10).sort((a, b) => a - b);
+console.log(sort);
+
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, залишивши тільки парні числа
+let filter = randomiser(20).filter(value => value % 2 === 0);
+console.log(filter);
+
 // - створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+let strings = randomiser(10).map(value => value + '');
+console.log(strings);
+
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
-//     let nums = [11,21,3];
+let nums = [11, 21, 3];
 // sortNums('ascending') // [3,11,21]
 // sortNums('descending') // [21,11,3]
-//
-//
+let sortNums = (arr, direction = 'ascending') => {
+    if (direction === 'ascending') {
+        return arr.sort((a, b) => a - b);
+    } else if (direction === 'descending') {
+        return arr.sort((a, b) => b - a);
+    } else {
+        return 'something wrong!!??'
+    }
+};
+console.log(sortNums(nums, 'ascending'));
+console.log(sortNums(nums, 'descending'));
+
 // - є масив
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
 // -- відсортувати його за спаданням за monthDuration
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
-//
-// - Напишите функцию cutString(str, n), которая делит строку на подстроки, состоящие из n символов.
-// document.writeln(cutString('наслаждение',3)) // [нас,лаж,ден,ие]
+let sort1 = coursesAndDurationArray
+    .filter(value => value.monthDuration > 5)
+    .sort((a, b) => b.monthDuration - a.monthDuration);
+console.log(sort1);
